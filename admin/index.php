304c61eb9a382a4b830531aae6b0aa909fe1fb19
elecctroman <55356201+elecctroman@users.noreply.github.com>
2025-10-16 23:39:54 +0300

    Delete admin directory

--- admin/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__ . '/../bootstrap.php';
-
-use App\Helpers;
-use App\Auth;
-
-Auth::requireRoles(Auth::adminRoles(), '/admin/login.php');
-
-Helpers::redirect('/admin/dashboard.php');
